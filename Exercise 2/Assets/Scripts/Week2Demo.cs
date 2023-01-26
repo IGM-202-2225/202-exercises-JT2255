e50c8d85ed50c80dbcb9dd3fe139dae1f108fa92
Joe <emailforinterwebs.jt@gmail.com>
2023-01-26 11:44:23 -0500

    added script and built exercise 2

--- Exercise 2/Assets/Scripts/Week2Demo.cs
+++ Exercise 2/Assets/Scripts/Week2Demo.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Week2Demo : MonoBehaviour
 {
     [SerializeField]
-    int health = 5;
+    //int health = 5;
 
     public GameObject snowman;
 
@@ -13,18 +13,20 @@ public class Week2Demo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < health; i++)
+       /* for (int i = 0; i < health; i++)
         {
             Instantiate(snowman, new Vector3(0, i, 0), new Quaternion());
         }
+       */
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(health);
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x + 0.0005f, gameObject.transform.position.y, gameObject.transform.position.z);
-    }
+        gameObject.transform.position = new Vector3(Mathf.PingPong(Time.time*1.5f, 4.02f), transform.position.y, transform.position.z);
 
-    
+        if (gameObject.transform.position.x >= 4){
+            Instantiate(snowman, new Vector3(Random.Range(-22f, 22f), Random.Range(0, 4f), Random.Range(-15f, 20f)), new Quaternion());
+        }
+    } 
 }
